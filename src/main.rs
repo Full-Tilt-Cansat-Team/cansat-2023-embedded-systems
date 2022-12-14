31a618f513901217a62e9db1cbb6e2950a7407d7
thelenlucas <89479728+thelenlucas@users.noreply.github.com>
2022-12-14 14:51:22 -0600

    Spinlocked telemetry bugfixing

--- src/main.rs
+++ src/main.rs
@@ -14,11 +14,9 @@ use rp2040_hal::clocks::Clock;
 const XTAL_FREQ_HZ: u32 = 12_000_000u32;
 
 //Spinlocks for communication between cores
-use rp2040_hal::sio::Spinlock0;
+#[allow(unused_imports)]
+use rp2040_hal::sio::Spinlock0; //Debug Spinlock
 use rp2040_hal::sio::Spinlock1; //Telemetry spinlock
-
-//LED Container
-static mut LED_ON: u32 = 0;
 
 //Enums that handle some data transfer
 
@@ -43,103 +41,58 @@ enum FlightState {
     PostFlight,
 }
 
-//Struct to contain telemetry data for passing between cores
-struct TelemetryStructure {
-    //Team ID
-    team_id: u16,
-
-    //Mission time in hours, minutes, seconds
-    mission_time_hours: u8,
-    mssion_time_minutes: u8,
-    mission_time_seconds: f32,
-
-    //Packet count
-    packet_count: u16,
-
-    //Mode (encoded as a u8)
-    mode: ContainerMode,
-
-    //State
-    state: FlightState,
-
-    //Altitude in meters (pressure-derived)
-    altitude: f32,
-
-    //Heat shield deployment state
-    heat_shield_deployment_state: DeploymentState,
-
-    //Parachute deployment state
-    parachute_deployment_state: DeploymentState,
-
-    //Mast deployment state
-    mast_deployment_state: DeploymentState,
-
-    //Temperature in degrees Celsius
-    temperature: f32,
-
-    //Voltage in volts
-    voltage: f32,
-
-    //GPS Time in hours, minutes, seconds
-    gps_time_hours: u8,
-    gps_time_minutes: u8,
-    gps_time_seconds: f32,
-
-    //GPS Latitude in degrees
-    gps_latitude: f32,
-
-    //GPS Longitude in degrees
-    gps_longitude: f32,
-
-    //GPS Altitude in meters
-    gps_altitude: f32,
-
-    //GPS Sat Count
-    gps_sat_count: u8,
-
-    //Last command received (as a fixed-length u8 array)
-    last_command_received: [u8; 32],
-}
-
-static mut TELEMETRY: TelemetryStructure = TelemetryStructure {team_id: 0,
-    mission_time_hours: 0,
-    mssion_time_minutes: 0,
-    mission_time_seconds: 0.0,
-    packet_count: 0,
-    mode: ContainerMode::Flight,
-    state: FlightState::PreFlight,
-    altitude: 0.0,
-    heat_shield_deployment_state: DeploymentState::Undeployed,
-    parachute_deployment_state: DeploymentState::Undeployed,
-    mast_deployment_state: DeploymentState::Undeployed,
-    temperature: 0.0,
-    voltage: 0.0,
-    gps_time_hours: 0,
-    gps_time_minutes: 0,
-    gps_time_seconds: 0.0,
-    gps_latitude: 0.0,
-    gps_longitude: 0.0,
-    gps_altitude: 0.0,
-    gps_sat_count: 0,
-    last_command_received: [0; 32],
-};   
+//Spinlocked global telemetry fields
+#[allow(dead_code)]
+static mut TEAM_ID: u32 = 0;
+#[allow(dead_code)]
+static mut MISSION_TIME_HOURS: u32 = 0;
+#[allow(dead_code)]
+static mut MISSION_TIME_MINUTES: u32 = 0;
+#[allow(dead_code)]
+static mut MISSION_TIME_SECONDS: f32 = 0.0;
+#[allow(dead_code)]
+static mut PACKET_COUNT: u32 = 0;
+#[allow(dead_code)]
+static mut CONTAINER_MODE: ContainerMode = ContainerMode::Flight;
+#[allow(dead_code)]
+static mut MISSION_STATE: FlightState = FlightState::PreFlight;
+#[allow(dead_code)]
+static mut ALTITUDE: f32 = 0.0;
+#[allow(dead_code)]
+static mut HEAT_SHIELD_DEPLOYMENT_STATE: DeploymentState = DeploymentState::Undeployed;
+#[allow(dead_code)]
+static mut TEMPERATURE: f32 = 0.0;
+#[allow(dead_code)]
+static mut VOLTAGE: f32 = 0.0;
+#[allow(dead_code)]
+static mut GPS_TIME_HOURS: u32 = 0;
+#[allow(dead_code)]
+static mut GPS_TIME_MINUTES: u32 = 0;
+#[allow(dead_code)]
+static mut GPS_TIME_SECONDS: f32 = 0.0;
+#[allow(dead_code)]
+static mut GPS_LATITUDE: f32 = 0.0;
+#[allow(dead_code)]
+static mut GPS_LONGITUDE: f32 = 0.0;
+#[allow(dead_code)]
+static mut GPS_SAT_COUNT: u32 = 0;
+#[allow(dead_code)]
+static mut CMD_ECHO: [u32; 32] = [0; 32];
 
 //Function to update the LED state
 fn blink_led() {
     //Claim the spinlock
-    let _lock = Spinlock0::claim();
+    let _lock = Spinlock1::claim();
 
     //Now this code is inside an unsafe block to avoid,
     //the compiler from yelling at us for modifying this
     //from multiple cores
     unsafe {
-        //If the LED is on, turn it off
-        if LED_ON == 1 {
-            LED_ON = 0;
+        if TEAM_ID == 0 {
+            TEAM_ID = 1;
         }
-        //Otherwise, turn it on
         else {
-            LED_ON = 1;
+            TEAM_ID = 0;
         }
     }
 }
@@ -163,38 +116,11 @@ fn core1_task () -> ! {
 
     let mut led_pin = pins.gpio25.into_push_pull_output();
 
-    //Claim the telemetry spinlock, and initialize the telemetry structure to 0 for all fields
-    let _lock = Spinlock1::claim();
-
-    unsafe {
-        TELEMETRY.team_id = 0;
-        TELEMETRY.mission_time_hours = 0;
-        TELEMETRY.mssion_time_minutes = 0;
-        TELEMETRY.mission_time_seconds = 0.0;
-        TELEMETRY.packet_count = 0;
-        TELEMETRY.mode = ContainerMode::Flight;
-        TELEMETRY.state = FlightState::PreFlight;
-        TELEMETRY.altitude = 0.0;
-        TELEMETRY.heat_shield_deployment_state = DeploymentState::Undeployed;
-        TELEMETRY.parachute_deployment_state = DeploymentState::Undeployed;
-        TELEMETRY.mast_deployment_state = DeploymentState::Undeployed;
-        TELEMETRY.temperature = 0.0;
-        TELEMETRY.voltage = 0.0;
-        TELEMETRY.gps_time_hours = 0;
-        TELEMETRY.gps_time_minutes = 0;
-        TELEMETRY.gps_time_seconds = 0.0;
-        TELEMETRY.gps_latitude = 0.0;
-        TELEMETRY.gps_longitude = 0.0;
-        TELEMETRY.gps_altitude = 0.0;
-        TELEMETRY.gps_sat_count = 0;
-        TELEMETRY.last_command_received = [0; 32];
-    }
-
     loop {
         //Claim the spinlock
-        let _lock = Spinlock0::claim();
+        let _lock = Spinlock1::claim();
         unsafe {
-            if LED_ON == 1 {
+            if TEAM_ID == 0 {
                 led_pin.set_high().unwrap();
             }
             else {
@@ -217,10 +143,6 @@ fn main () -> ! {
     let cores = mc.cores();
     let core1 = &mut cores[1];
 
-    let _run = core1.spawn(unsafe { &mut CORE1_STACK.mem }, move || {
-        core1_task()
-    });
-
     //Set up our watchdog timer, though we won't be using it
     let mut watchdog = hal::Watchdog::new(pac.WATCHDOG);
 
@@ -237,6 +159,10 @@ fn main () -> ! {
 
     //Delay object lets us control timing
     let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
+
+    let _run = core1.spawn(unsafe { &mut CORE1_STACK.mem }, move || {
+        core1_task()
+    });
 
     loop {
         blink_led();
